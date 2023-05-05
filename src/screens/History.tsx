45d17e4f019cb1ada8134fdbd8b339a6a7115a13
DanielFrancajp <danieldenotas@gmail.com>
2023-05-05 16:18:57 -0300

    create component exercirseCard and HistoryCard, ScreenHeader

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,12 +1,48 @@
-import {
-    Center,
-    Text
-} from "native-base";
+import { useState } from 'react';
+import { Heading, VStack, SectionList, Text, Center } from "native-base";
+
+import { ScreenHeader } from "@components/ScreenHeader";
+import { HistoryCardy } from "@components/HistoryCard";
+
+
 
 export function History() {
+
+    const [exercises, setExercises] = useState([
+        {
+            title: '05.05.2023',
+            data: ['Puxada Frontal', 'Remada unilateral']
+        },
+        {
+            title: '04.04.2023',
+            data: ['Biceps']
+        },
+    ]);
+
     return (
-        <Center flex={1}>
-            <Text color="white">History</Text>
-        </Center>
+        <VStack flex={1}>
+            <ScreenHeader title="Histórico de Exercícios" />
+            <SectionList
+                sections={exercises}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <HistoryCardy />
+                )}
+                renderSectionHeader={({ section }) => (
+                    <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+                        {section.title}
+                    </Heading>
+                )}
+                px={8}
+                contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: 'center' }}
+                ListEmptyComponent={() => (
+                    <Text color="gray.100" textAlign="center" >
+                        Não há exercícios registrados ainda. {'\n'}
+                        Vamos fazer exercícios hoje?
+                    </Text>
+                )}
+            />
+
+        </VStack>
     )
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,13 +1,16 @@
 import { useState } from 'react';
-import { VStack, FlatList } from "native-base";
+import { VStack, FlatList, HStack, Heading, Text, } from "native-base";
 
 import { HomeHeader } from "@components/HomeHeader";
 import { Group } from "@components/Group";
+import { ExerciseCard } from '@components/ExerciseCard';
 
 export function Home() {
 
-    const [groups, setGroups] = useState(['costa', 'biceps', 'triceps', 'ombro',]);
-    const [groupSelected, setGroupSelected] = useState('costa');
+    const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'ombro',]);
+    const [exercises, setExercises] = useState(['Puxada Frontal', 'Remada curvada', 'Remada unilateral', 'Levantamento terra']);
+    const [groupSelected, setGroupSelected] = useState('Costas');
+
 
     return (
         <VStack flex={1}>
@@ -18,7 +21,7 @@ export function Home() {
                 renderItem={({ item }) => (
                     <Group
                         name={item}
-                        isActive={groupSelected === item}
+                        isActive={groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase()}
                         onPress={() => setGroupSelected(item)}
                     />
                 )}
@@ -29,7 +32,30 @@ export function Home() {
                 maxH={10}
             />
 
+            <VStack flex={1} px={8}>
 
+                <HStack justifyContent="space-between" mb={5}>
+                    <Heading color="gray.200" fontSize="md">
+                        Exercícios
+                    </Heading>
+
+                    <Text color="gray.200" fontSize="sm">
+                        {exercises.length}
+                    </Text>
+                </HStack>
+
+                <FlatList
+                    data={exercises}
+                    keyExtractor={item => item}
+                    renderItem={({ item }) => (
+                        <ExerciseCard
+
+                        />
+                    )}
+                    showsVerticalScrollIndicator={false}
+                    _contentContainerStyle={{ paddingBottom: 20 }}
+                />
+            </VStack>
 
 
         </VStack>
